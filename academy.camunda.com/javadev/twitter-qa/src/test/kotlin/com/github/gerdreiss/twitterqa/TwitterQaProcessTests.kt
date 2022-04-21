70423f59c25b104e848ba7c2daa1ba4b80387f46
Gerd Reiss <gerd@reiss.pro>
2022-04-21 18:43:15 +0200

    [Camunda Platform for Java Developers] Twitter QA - test fixed with a mock

--- academy.camunda.com/javadev/twitter-qa/src/test/kotlin/com/github/gerdreiss/twitterqa/TwitterQaProcessTests.kt
+++ academy.camunda.com/javadev/twitter-qa/src/test/kotlin/com/github/gerdreiss/twitterqa/TwitterQaProcessTests.kt
@@ -1,15 +1,30 @@
 package com.github.gerdreiss.twitterqa
 
+import org.camunda.bpm.engine.delegate.DelegateExecution
+import org.camunda.bpm.engine.delegate.JavaDelegate
 import org.camunda.bpm.engine.test.Deployment
 import org.camunda.bpm.engine.test.assertions.ProcessEngineTests
 import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests
+import org.camunda.bpm.engine.test.mock.Mocks
 import org.camunda.bpm.extension.process_test_coverage.junit5.ProcessEngineCoverageExtension
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.slf4j.LoggerFactory
 import kotlin.random.Random
 
 @ExtendWith(ProcessEngineCoverageExtension::class)
 class TwitterQaProcessTests {
+
+    @BeforeEach
+    fun init() {
+        Mocks.register("createTweetDelegate", object : JavaDelegate {
+            val logger = LoggerFactory.getLogger(javaClass)
+            override fun execute(execution: DelegateExecution) {
+                logger.info("Sending tweet: ${execution.getVariable("content")}")
+            }
+        })
+    }
 
     @Test
     @Deployment(resources = ["processes/twitter-qa.bpmn"])
