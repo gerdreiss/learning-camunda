fecebc6c8746d6d56a73955db9c307cb1308dbde
Gerd Reiss <gerd@reiss.pro>
2022-04-21 18:40:04 +0200

    [Camunda Platform for Java Developers] Twitter QA - the test fails with "Unknown property used in expression: ${createTweetDelegate}. Cause: Cannot resolve identifier 'createTweetDelegate'"

--- academy.camunda.com/javadev/twitter-qa/src/main/kotlin/com/github/gerdreiss/twitterqa/delegates/CreateTweetDelegate.kt
+++ academy.camunda.com/javadev/twitter-qa/src/main/kotlin/com/github/gerdreiss/twitterqa/delegates/CreateTweetDelegate.kt
@@ -1,20 +1,23 @@
-package com.github.gerdreiss.twitterqa.delegate
+package com.github.gerdreiss.twitterqa.delegates
 
 import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.delegate.JavaDelegate
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import twitter4j.TwitterFactory
 import twitter4j.auth.AccessToken
 
-@Component
+@Component("createTweetDelegate")
 class CreateTweetDelegate : JavaDelegate {
 
-    val logger: Logger = LoggerFactory.getLogger(javaClass)
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        @JvmStatic
+        private val logger = LoggerFactory.getLogger(javaClass.enclosingClass)
+    }
 
     override fun execute(execution: DelegateExecution) {
-        val content = "I did it! Cheers G"
+        val content = execution.getVariable("content") as String
         logger.info("Publishing tweet: '$content'")
         val accessToken = AccessToken(
             "220324559-CO8TfUmrcoCrvFHP4TacgToN5hLC8cMw4n2EwmHo",
@@ -23,6 +26,6 @@ class CreateTweetDelegate : JavaDelegate {
         val twitter = TwitterFactory().instance
         twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk", "gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS")
         twitter.oAuthAccessToken = accessToken
-        //twitter.updateStatus(content)
+        twitter.updateStatus(content)
     }
 }
--- academy.camunda.com/javadev/twitter-qa/src/test/kotlin/com/github/gerdreiss/twitterqa/TwitterQaProcessTests.kt
+++ academy.camunda.com/javadev/twitter-qa/src/test/kotlin/com/github/gerdreiss/twitterqa/TwitterQaProcessTests.kt
@@ -3,17 +3,21 @@ package com.github.gerdreiss.twitterqa
 import org.camunda.bpm.engine.test.Deployment
 import org.camunda.bpm.engine.test.assertions.ProcessEngineTests
 import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests
-import org.camunda.bpm.extension.junit5.test.ProcessEngineExtension
+import org.camunda.bpm.extension.process_test_coverage.junit5.ProcessEngineCoverageExtension
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import kotlin.random.Random
 
-@ExtendWith(ProcessEngineExtension::class)
+@ExtendWith(ProcessEngineCoverageExtension::class)
 class TwitterQaProcessTests {
 
     @Test
     @Deployment(resources = ["processes/twitter-qa.bpmn"])
     fun testHappyPath() {
-        val variables = mapOf("approved" to true)
+        val variables = mapOf(
+            "approved" to true,
+            "content" to "Exercise 4 test - 0x" + Random.nextBits(16)
+        )
         val processInstance = BpmnAwareTests.runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables)
         ProcessEngineTests.assertThat(processInstance).isEnded
     }
